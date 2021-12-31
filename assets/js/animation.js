6433b0eca256cdc651cb64a56b17bb794ef76787
박혜정 <2017029716@hanyang.ac.kr>
2021-12-31 14:00:51 +0900

    animation 수정

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,13 +1,23 @@
 var doctorPosition;
+var locationPosition;
+let marginOffset = 430;
 
 window.addEventListener("load", function () {
   doctorPosition = document
     .getElementById("doctor-wrapper")
     .getBoundingClientRect().top;
+
+  locationPosition = document
+    .getElementById("location")
+    .getBoundingClientRect().top;
+
+  document.getElementById("doctor-wrapper").style.opacity = 0;
+  document.getElementById("location").style.opacity = 0;
 });
 
 window.addEventListener("scroll", function () {
-  if (doctorPosition <= window.scrollY + 300) {
+  if (doctorPosition <= window.scrollY + marginOffset) {
+    document.getElementById("doctor-wrapper").style.opacity = 1;
     document
       .getElementById("main-text")
       .classList.add("animate__animated", "animate__slideInUp");
@@ -20,5 +30,12 @@ window.addEventListener("scroll", function () {
     document
       .getElementById("history")
       .classList.add("animate__animated", "animate__fadeIn");
+  }
+
+  if (locationPosition <= window.scrollY + marginOffset) {
+    document.getElementById("location").style.opacity = 1;
+    document
+      .getElementById("locationText")
+      .classList.add("animate__animated", "animate__slideInRight");
   }
 });
